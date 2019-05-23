2fd3d5e8c1f821d03edd90e28c9f6972d269e783
bvanberl <bvanberl@uwo.ca>
2019-05-22 23:24:11 -0400

    Listview changes - button fixes, style changes

--- app/src/main/java/com/vanberlo/blake/newname_android/Adapters/RecentsListAdapter.java
+++ app/src/main/java/com/vanberlo/blake/newname_android/Adapters/RecentsListAdapter.java
@@ -2,6 +2,8 @@ package com.vanberlo.blake.newname_android.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -66,7 +68,7 @@ public class RecentsListAdapter extends ArrayAdapter<Name> {
             @Override
             public void onClick(View v) {
                 Integer pos = (Integer)v.getTag();
-                onFavouriteBtnClicked(pos);
+                onFavouriteBtnClicked(pos, v);
             }
         });
         shareBtn.setOnClickListener(new View.OnClickListener(){
@@ -80,10 +82,14 @@ public class RecentsListAdapter extends ArrayAdapter<Name> {
     }
 
 
-    public void onFavouriteBtnClicked(int idx){
+    public void onFavouriteBtnClicked(int idx, View v){
         String selectedName = data.get(idx).getName();
         Gender gender = data.get(idx).getGender();
         realmService.insertName( selectedName, gender);
+
+        ImageButton favBtn = (ImageButton)v;
+        favBtn.setEnabled(false);
+        favBtn.setImageResource(R.drawable.icon_favourites_purple);
 
         CharSequence text = selectedName + " saved to Favourites!";
         int duration = Toast.LENGTH_SHORT;
